dfa0bd4dd48603501fd7c286d3376a0801406344
Nikita <nikkumanral44308@gmail.com>
2024-01-07 19:08:06 +0530

    lets added mvc applicatiomn

--- WebBookStore/WebBookStore/Program.cs
+++ WebBookStore/WebBookStore/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+// We can use both method for routing
+app.MapDefaultControllerRoute();
+//app.MapControllerRoute("Deafault", "{controller=Home}/{Action=Index}/{id}");
+
+//app.MapGet("/", () => "Hello Nikita");
+
+
 
 app.Run();
